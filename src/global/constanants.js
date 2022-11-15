cfe218f6b356e86df64dc4011292744beac07175
Amir-abbasy-k <amirabbasyk@gmail.com>
2022-11-15 22:40:28 +0530

    switch to new branch

--- src/global/constanants.js
+++ src/global/constanants.js
@@ -1,4 +1,11 @@
-export const default_url = "https://coupen.herokuapp.com";
+// export const default_url = "https://coupen.herokuapp.com";
+
+// live
+export const default_url = "http://54.152.5.122:4000";
+
+// localhost
+// export const default_url = "http://192.168.0.103:4000";
+
 
 export const colors = {
     // primary: '#716AFA'
@@ -11,7 +18,9 @@ export const fonts = {
 
 }
 
+// amir
 export const razorpay_key = "rzp_test_vlFzTMVZ121Jw1";
+// export const stripe_pub_key = "pk_test_51LfrOOBjRYqL3JogUGeZO2zgbaC2Enj6HMhNL131FuoIjLltWPwv8otxRJbM9SCL7zkz4BXZrdNPzKbkMDDQruiE00iqSuEug8";
+
+// espera
 export const stripe_pub_key = "pk_test_51H2711DqIVpJBt3RNYIncduJKSJPxTwEYygd4qrZYO34egsxttr06XhTnbZV37mXlAQFqNt2bSvvW2Rvemdn95d3006HwkMWBM";
-
-
